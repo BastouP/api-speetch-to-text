65e3e73aecc6995784f86fee180b25ebd1f4a712
BastouP <bastou.poujois@gmail.com>
2024-09-21 19:35:45 +0200

    Update speech.js

--- api/speech.js
+++ api/speech.js
@@ -1,1 +1,9 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    // L'API Web Speech ne peut pas fonctionner ici car elle nécessite un navigateur
+    return res.status(400).json({ message: 'L\'API Web Speech doit être utilisée côté client (navigateur).' });
+  } else {
+    res.status(405).json({ message: 'Méthode non autorisée' });
+  }
+}
 
